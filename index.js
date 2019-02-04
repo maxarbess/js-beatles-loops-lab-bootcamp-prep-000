b512268bfc98e271d3f7bdbbdefcc4953eeb80aa
Max Arbess <maxarbess1@gmail.com>
2019-02-04 20:32:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// add solution here
+function theBeatlesPlay(musicians,instruments)  {
+  var beatlesPlayers = [];
+  for (var i=0; i < musicians.length; i++)  {
+    beatlesPlayers.push(musicians[i] + " plays " 
+    + instruments[i])
+  }
+  return beatlesPlayers
+}
+function johnLennonFacts(array) {
+const facts = [];
+  var i = 0;
+  while (i < array.length)  {
+    array.push(facts[i] + "!!!");
+    i++;
+  }
+  return array
+}
